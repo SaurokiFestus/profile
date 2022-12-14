df6ed4c61662712fe1bad016c090da15f1793b5e
Sauroki <saurokisaigilu.45@gmail.com>
2022-12-15 00:25:36 +0300

    commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Moringa Overflow",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"BillAdd",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"StockApp",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"i-muvi",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
